78bb3cc29737f692a50d9cc8b1c0d0ff364f1b52
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-02-24 22:13:08 +0530

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      signInWithEmailAndPassword(auth, email, password);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Anshul</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
 
           <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
         </form>
-        <p>You don't have an account? Register</p>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Add from "../img/addAvatar.png";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -61,7 +61,9 @@ const Register = () => {
           <button>Sign up</button>
           {err && <span>Something went wrong</span>}
         </form>
-        <p>You do have an account? Login</p>
+        <p>
+          You do have an account? <Link to="/login">Login</Link>
+        </p>
       </div>
     </div>
   );
